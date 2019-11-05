e0012ec717b3e84c404abccb43d13ef4537ba038
Niko Virolainen <niko.virolainen@hotmail.com>
2019-11-05 11:37:39 +0200

    Added bottomstacknavigator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FolderIcon from "@material-ui/icons/Folder";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 import "./App.css";
 import Customers from "./Customers";
 import Calendar from "./Calendar";
 
-function App() {
+const useStyles = makeStyles({
+	root: {
+		width: 500
+	}
+});
+
+const App = () => {
+	const classes = useStyles();
+	const [value, setValue] = React.useState(0);
+
+	const handleChange = (event, newValue) => {
+		setValue(newValue);
+	};
 	return (
 		<div>
-			<Customers />
 			<div>
-				<Router>
-					<div>
-						<Route exact path="/"></Route>
-						<Route path="/calendar">
-							<Calendar />
-						</Route>
-					</div>
-				</Router>
+				<Customers />
 			</div>
+			<BottomNavigation value={value} onChange={handleChange} className={classes.root}>
+				<BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+				<BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+				<BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+				<BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+			</BottomNavigation>
 		</div>
 	);
-}
+};
 
 export default App;
