bd58dc14c58ace16d217b63fb92453461de01338
Niko Virolainen <niko.virolainen@hotmail.com>
2019-11-14 11:23:46 +0200

    Styling changes etc

--- src/App.js
+++ src/App.js
@@ -10,53 +10,62 @@ import Customers from "./components/Customers";
 import Calendar from "./components/Calendar";
 
 const App = () => {
-	const classes = useStyles();
-	const [value, setValue] = React.useState(0);
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
 
-	return (
-		<div>
-			<BrowserRouter>
-				<div>
-					<div style={{ textAlign: "center", marginTop: 1 }}>
-						<Switch>
-							<Route path="/customers" component={Customers} />
-							<Route path="/calendar" component={Calendar} />
-							<Route render={() => <h1>Page not found</h1>} />
-						</Switch>
-					</div>
-					<div>
-						<BottomNavigation
-							value={value}
-							onChange={(event, newValue) => {
-								setValue(newValue);
-							}}
-							showLabels
-							className={classes.root}
-						>
-							<BottomNavigationAction
-								to="/customers"
-								component={Link}
-								label="Customers"
-								icon={<FitnessCenterIcon />}
-							/>
-							<BottomNavigationAction
-								to="/calendar"
-								component={Link}
-								label="Calendar"
-								icon={<TodayIcon />}
-							/>
-						</BottomNavigation>
-					</div>
-				</div>
-			</BrowserRouter>
-		</div>
-	);
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <div style={{ textAlign: "center", marginTop: 1 }}>
+            <Switch>
+              <Route path="/customers" component={Customers} />
+              <Route path="/calendar" component={Calendar} />
+              <Route path="/" component={Customers} />
+              <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
+          </div>
+          <div>
+            <BottomNavigation
+              value={value}
+              onChange={(event, newValue) => {
+                setValue(newValue);
+              }}
+              showLabels
+              className={classes.root}
+            >
+              <BottomNavigationAction
+                to="/customers"
+                component={Link}
+                label="Customers"
+                icon={<FitnessCenterIcon />}
+                className={classes.root}
+              />
+              <BottomNavigationAction
+                to="/calendar"
+                component={Link}
+                label="Calendar"
+                icon={<TodayIcon />}
+                className={classes.root}
+              />
+            </BottomNavigation>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 const useStyles = makeStyles({
-	root: {
-		width: 2600
-	}
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px"
+  }
 });
 
 export default App;
